1478683a50e975af7b6fba1665d3918ae9743b0d
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-14 17:47:01 -0300

    Project created

---
+++ src/main/java/com/example/workshopsbmongo/WorkshopSbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopsbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSbMongodbApplication.class, args);
+	}
+
+}
