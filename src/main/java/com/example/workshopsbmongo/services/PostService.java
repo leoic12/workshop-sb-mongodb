49934636fedee94f656818f75ede4a7901b15219
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-15 17:20:40 -0300

    Obtaining post by id

---
+++ src/main/java/com/example/workshopsbmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.workshopsbmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopsbmongo.domain.Post;
+import com.example.workshopsbmongo.repository.PostRepository;
+import com.example.workshopsbmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
