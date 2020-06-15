d6258db95b500f18f4e8fd66e7cf6e3e64513bb8
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-15 18:03:46 -0300

    @query

--- src/main/java/com/example/workshopsbmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopsbmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.workshopsbmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopsbmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.example.workshopsbmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContaining(String text);
 }
--- src/main/java/com/example/workshopsbmongo/services/PostService.java
+++ src/main/java/com/example/workshopsbmongo/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContaining(text);
+		return repo.findByTitle(text);
 	}
 
 }
